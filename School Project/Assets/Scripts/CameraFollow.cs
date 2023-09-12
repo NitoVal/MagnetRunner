c75a5a43dba62aea56ddc011fcf4c46a03ae2397
NitoVal <nitoval360@gmail.com>
2023-09-12 08:57:09 -0400

    Merge to Main

---
+++ School Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector3 offset = new Vector3(0f, 2f, -10f);
+    private float smoothTime = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+
+    private Transform target;
+
+    void Update()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        if (target == null)
+            return;
+
+        Vector3 targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
---
+++ School Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float x; 
+
+    float speed = 10f;
+    float jumpForce = 16f;
+    bool isFacingRight = true;
+
+    Rigidbody2D rb;
+
+    float grCheckRadius = 0.1f;
+    Transform grCheck;
+    LayerMask grMask;
+
+    PlayerInput input;
+    public PlayerInputActions actions;
+
+    Vector2 mousePos;
+    [SerializeField] Transform point;
+
+    //Called when instance is loaded in the scene
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        input = GetComponent<PlayerInput>();
+        grCheck = gameObject.transform.GetChild(0);
+        grMask = LayerMask.GetMask("Ground");
+
+        actions = new PlayerInputActions();
+        actions.Enable();
+        actions.Player.Jump.performed += Jump;
+        actions.Player.Crouch.performed += Crouch;
+        actions.Player.Crouch.canceled += CancelCrouch;
+    }
+
+    private void Update()
+    {
+        x = actions.Player.Movement.ReadValue<float>();
+        mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        if (x > 0 && !isFacingRight)
+            Flip();
+        if (x < 0 && isFacingRight)
+            Flip();
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(x * speed, rb.velocity.y);
+    }
+    private void LateUpdate()
+    {
+        Vector2 dir = mousePos - (Vector2)point.position;
+        float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg - 90f;
+        point.rotation = Quaternion.Euler(0f,0f,angle);
+    }
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0, 180f, 0);
+    }
+    private bool isGrounded() { return Physics2D.OverlapCircle(grCheck.position, grCheckRadius, grMask); }
+    private void Jump(InputAction.CallbackContext obj)
+    {
+        //play animation
+        if (isGrounded())
+            rb.velocity = Vector2.up * jumpForce;
+    }
+    private void Crouch(InputAction.CallbackContext obj)
+    {
+        //play animation
+        transform.localScale -= new Vector3(0, 0.5f, 0);
+    }
+    private void CancelCrouch(InputAction.CallbackContext obj)
+    {
+        //play animation
+        transform.localScale += new Vector3(0,.5f,0);
+    }
+}
